91e2a41adfb58a02383189ef1db0fb5bdc53f7e2
Carrilhos <gcarrilhos@gmail.com>
2020-09-30 12:47:28 -0300

    feat(listnavers and services): add list of navers from api

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,3 +3,5 @@ import client from 'providers/fetchClient'
 export const getUser = () => client.get('/v1/me')
 
 export const login = data => client.post('/v1/users/login', data)
+
+export const listarNavers = () => client.get('/v1/navers')
